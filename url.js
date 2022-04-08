df57c24d10265f6f6150c7049a101c082d542461
Riyaz <r-ahmed@riysaim.com>
2022-04-08 17:25:15 +0530

    updated index.html and corresponding css file

---
+++ url.js
@@ -0,0 +1,5 @@
+1. video-1
+<iframe width="auto" height="315" src="https://www.youtube.com/embed/8JJ101D3knE" title="YouTube video player" frameborder="0"
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+2. video-2
