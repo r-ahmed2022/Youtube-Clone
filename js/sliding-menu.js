b49e7eb9ce5735a2555a5aaad2811ef0fe46f407
Riyaz <r-ahmed@riysaim.com>
2022-04-09 05:18:30 +0530

    updated index.html , corresponding css,and added little Javacript for slidimg menu

---
+++ js/sliding-menu.js
@@ -0,0 +1,14 @@
+function openSlidingDiv() {
+  document.getElementById("sliding-menu-items").style.width = "20%";
+  document.getElementById("container").style.backgroundColor="black";
+  document.body.style.color = "white";
+  document.getElementsByClassName('mic').style.backgroundColor ="white";
+
+}
+
+function closeSlidingDiv() {
+  document.getElementById("sliding-menu-items").style.width = "0%";
+  document.getElementById("container").style.backgroundColor="white";
+  document.body.style.color = "black";
+  document.getElementById("icon-section").style.backgroundColor="white";
+}
